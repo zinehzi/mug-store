e88cbb2dcf0fa71018aaa04d48a2882dd119eb7f
Zinat Ziaraty <z.ziaraty@asax.local>
2023-10-11 19:19:02 +0330

    Adding Products To Cart

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import { fetchProducts } from "./api/products.js";
 
-async function displayProducts(products) {
-  const productList = document.getElementById("product-list");
+let cart = [];
+let count = 0;
+
+const productList = document.getElementById("product-list");
+const cartIconCount = document.getElementById("cart-icon-count");
+
+const displayProducts = (products) => {
   for (let product of products) {
-    const productDiv = document.createElement("Div");
+    const productDiv = document.createElement("div");
     productDiv.className = "product";
     const productImg = document.createElement("Img");
     productImg.src = `../images/${product.image}`;
@@ -14,13 +19,41 @@ async function displayProducts(products) {
     productPrice.textContent = `${product.price} تومان`;
     const productBtn = document.createElement("button");
     productBtn.type = "button";
+    productBtn.id = "btn";
     productBtn.textContent = "افزودن به سبد خرید";
+    const buttonIcon = document.createElement("i");
+    buttonIcon.classList.add("fa", "fa-shopping-cart");
+    productBtn.appendChild(buttonIcon);
+    productBtn.onclick = () => addToCart(product);
     productCaption.appendChild(productTitle);
     productCaption.appendChild(productPrice);
     productDiv.appendChild(productImg);
     productDiv.appendChild(productCaption);
     productDiv.appendChild(productBtn);
     productList.appendChild(productDiv);
+  }
+};
+
+let cartCount = 0;
+
+function addToCart(product) {
+  if (!cart.find((cartId) => cartId.id === product._id)) {
+    cart.push({
+      id: product._id,
+      name: product.name,
+      price: product.price,
+      quantity: count + 1,
+    });
+    cartIconCount.classList.add("cart-icon-count");
+    cartCount += 1;
+    cartIconCount.textContent = cartCount;
+  } else {
+    const cartItem = cart.find((cartItem) => cartItem.id === product._id);
+    cartItem.quantity += 1;
+    cartIconCount.classList.add("cart-icon-count");
+    cartCount += 1;
+
+    cartIconCount.textContent = cartCount;
   }
 }
 
@@ -30,3 +63,5 @@ async function render() {
 }
 
 render();
+
+export {cart};
---
+++ src/shopping-cart.js
@@ -0,0 +1,6 @@
+import { cart } from "./index.js";
+
+
+console.log("cart",cart);
+
+
